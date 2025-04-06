ac175a08b0d4d59aac3f63602a47292de89ea970
domnanob <domnanob@gmail.com>
2025-04-06 13:16:39 +0200

    Test result
    - Test result shown
    - Bug fixed in test page

--- BlazorApp2/BlazorApp2/wwwroot/Upload/44A4615D-7B5B-4966-A46A-08DD68AA55D6/e6c32dd1-b369-4692-53c2-08dd6c630a2c/NeptunkodProgram.cs
+++ BlazorApp2/BlazorApp2/wwwroot/Upload/44A4615D-7B5B-4966-A46A-08DD68AA55D6/e6c32dd1-b369-4692-53c2-08dd6c630a2c/NeptunkodProgram.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
-            string s = Console.ReadLine();
+            String s = Console.ReadLine();
             Console.WriteLine(s);
             Console.WriteLine(s);
         }
